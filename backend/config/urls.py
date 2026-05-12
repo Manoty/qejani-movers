d452badb6077830e7644af6b17057b627275c241
Kevin Manoti <kevinnoty21@gmail.com>
2026-05-12 16:18:51 +0300

    Wire into Root URLs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("api/auth/", include("apps.accounts.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/pricing/", include("apps.pricing.urls")),
 ]
